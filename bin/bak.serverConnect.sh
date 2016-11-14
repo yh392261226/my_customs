565552b871a34f2fb195e85800e790953e6c34c7
杨浩 <yh392261226@gmail.com>
2016-11-14 16:12:33 +0800

    add custom bin

--- bin/bak.serverConnect.sh
+++ bin/bak.serverConnect.sh
@@ -41,24 +41,6 @@ if { "$addr"=="home" } {
     set user [lindex root]
     set pwd [lindex bf37d82cdc57afa0d42d24cbcebcacdf8fca6280]
     set act [lindex {cd /home/web/v.sincaitest.com/auto/}]
-} elseif { "$addr"=="doing3" } {
-    set address [lindex 121.54.174.103]
-    set port [lindex 22]
-    set user [lindex root]
-    set pwd [lindex tgBhu.09]
-    set act [lindex {cd /data/app/www/}]
-#} elseif { "$addr"=="doing2" } {
-#    set address [lindex 103.20.221.237]
-#    set port [lindex 22]
-#    set user [lindex root]
-#    set pwd [lindex 1q2w3e4r5t6y]
-#    set act [lindex {cd /data/app/www/}]
-#} elseif { "$addr"=="doing" } {
-#    set address [lindex 174.139.202.7]
-#    set port [lindex 22]
-#    set user [lindex root]
-#    set pwd [lindex FrE3eyed]
-#    set act [lindex {cd /}]
 } elseif { "$addr"=="test" } {
     set address [lindex 192.168.146.133]
     set port [lindex 22]
@@ -66,12 +48,6 @@ if { "$addr"=="home" } {
     set pwd [lindex yanghao]
     set act [lindex {cd /}]
 } 
-#elseif {$addr==211} {
-#	set address [lindex 60.28.208.211]
-#	set port [lindex 7710]
-#	set user [lindex lianjun.yang]
-#	set pwd [lindex YangHao1017]
-#}
 spawn ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p$port $user@$address
 expect -re "password:" {
     send "$pwd\r"
--- bin/serverConnect.sh
+++ bin/serverConnect.sh
@@ -2,7 +2,7 @@
 set addr [lindex $argv 0]
 set dir  [lindex $argv 1]
 if { $addr=="" } {
-	puts "connectserver test|home|working|fenghuang|alienware";
+	puts "connectserver centos| locentos| locentos2";
 	exit;
 }
 if { "$addr"=="centos" } {
@@ -27,34 +27,6 @@ set act2 [lindex {}]
  set pwd [lindex yanghao1017]
  set act [lindex {}]
  set act2 [lindex {}]
-} elseif { "$addr"=="s1" } {
-set address [lindex 192.168.2.221]
-set port [lindex 22]
-set user [lindex root]
-set pwd [lindex admin123]
-set act [lindex {cd ~}]
-set act2 [lindex {}]
-} elseif { "$addr"=="s4" } {
-set address [lindex 192.168.1.224]
-set port [lindex 22]
-set user [lindex root]
-set pwd [lindex admin123]
-set act [lindex {cd ~}]
-set act2 [lindex {}]
-} elseif { "$addr"=="s5" } {
-set address [lindex 192.168.1.225]
-set port [lindex 22]
-set user [lindex root]
-set pwd [lindex admin123]
-set act [lindex {cd ~}]
-set act2 [lindex {}]
-} elseif { "$addr"=="shanghai" } { #web2
-set address [lindex 222.73.44.152]
-set port [lindex 8792]
-set user [lindex root]
-set pwd [lindex Maiyuan(%@&]
-set act [lindex {}]
-set act2 [lindex {}]
 }
 spawn ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p$port $user@$address
 expect -re "password:" {
