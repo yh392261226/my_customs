823853ba42d9116c448a01321ab7f1b1b3d1aa10
杨浩 <yh392261226@gmail.com>
2016-11-22 16:12:00 +0800

    update new function

--- my_shell/my_func.sh
+++ my_shell/my_func.sh
@@ -468,3 +468,37 @@ rmext () {
 function battery_charge {
   echo `~/bin/batcharge.py`
 }
+
+mcdf () {  # short for cdfinder
+  cd "`osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)'`"
+}
+
+function mla() {
+   ls -l  "$@" | awk '
+    {
+      k=0;
+      for (i=0;i<=8;i++)
+        k+=((substr($1,i+2,1)~/[rwx]/) *2^(8-i));
+      if (k)
+        printf("%0o ",k);
+      printf(" %9s  %3s %2s %5s  %6s  %s %s %s\n", $3, $6, $7, $8, $5, $9,$10, $11);
+    }'
+}
+
+mqfind () {
+  find . -exec grep -l -s $1 {} \;
+  return 0
+}
+
+mwhois() {
+  local domain=$(echo "$1" | awk -F/ '{print $3}') # get domain from URL
+  if [ -z $domain ] ; then
+    domain=$1
+  fi
+  echo "Getting whois record for: $domain …"
+
+  # avoid recursion
+          # this is the best whois server
+                          # strip extra fluff
+  /usr/bin/whois -h whois.internic.net $domain | sed '/NOTICE:/q'
+}
