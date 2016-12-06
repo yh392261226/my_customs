e0ca7b6f1d207e36ab92cdb6049e40b8d6d729b5
杨浩 <yh392261226@gmail.com>
2016-12-06 13:42:40 +0800

    update function change background in iterm2 for fish

---
+++ my_shell/fish/zmy_basic.fish
@@ -0,0 +1,34 @@
+#我自己的一些配置
+##设置一些变量
+set -gx MYRUNTIME (cat ~/.myruntime)
+set -gx MYPATH $MYRUNTIME
+set -gx USERNAME json
+set -gx GOPATH $HOME/go-develop
+set -gx GOOS darwin
+set -gx GOARCH amd64
+#set -U PATH /usr/local/opt/go/bin (brew --prefix homebrew/php/php70)/bin $GOPATH/bin /tools/ssh-auto-login/auto_gen $PATH
+#set -U $PATH $MYRUNTIME/customs/bin
+set -gx PATH $MYRUNTIME/customs/bin $PATH
+
+##别名设置
+alias goweb="godoc -http=:9900 >> /dev/null &"
+alias vimgo='vim -u ~/.vimrc.go'
+alias l="ls -a"
+alias lll="/usr/local/bin/ls++"
+alias gopl="open ~/Documents/golang/gopl-zh/_book/index.html"
+alias cl="clear"
+
+##图片脚本设置
+sh (cat ~/.myruntime)/tools/extendslocatetochangepicurl.sh
+
+##autojump设置
+[ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
+
+eval (thefuck --alias | tr '\n' ';')
+
+/usr/local/bin/screenfetch
+
+##引入iterm2 自动变更背景脚本
+if test -f $MYRUNTIME/customs/my_shell/fish/zmy_changebg.fish
+    source $MYRUNTIME/customs/my_shell/fish/zmy_changebg.fish
+end
