7d4cf8404aad9038c99fa5a03beba2a387daaa0d
杨浩 <yh392261226@gmail.com>
2017-03-06 12:47:54 +0800

    update php versions

--- my_shell/my_php.sh
+++ my_shell/my_php.sh
@@ -1,3 +1,4 @@
-export PATH="$(brew --prefix homebrew/php/php70)/bin:$(brew --prefix homebrew/php/php70)/sbin:$PATH"
+export PATH="$(brew --prefix homebrew/php/php71)/bin:$(brew --prefix homebrew/php/php71)/sbin:$PATH"
 ### add php-school
 export PATH="$PATH:/Users/json/.php-school/bin"
+source $(brew --prefix php-version)/php-version.sh && php-version 7
