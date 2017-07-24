052577701635539a681e1a3f2e73948a70bca4ec
杨浩 <yh392261226@gmail.com>
2017-07-24 15:05:39 +0800

    what am i do

--- my_shell/fish/zmy_basic.fish
+++ my_shell/fish/zmy_basic.fish
@@ -20,6 +20,47 @@ alias lll "/usr/local/bin/ls++"
 alias gopl "open ~/Documents/golang/gopl-zh/_book/index.html"
 alias cl "clear"
 
+##版本控制 git 别名设置
+alias gs "git status"
+alias gpl "git pull"
+alias gp 'git push'
+alias gps "git push"
+alias gsh "git show"
+alias gith "git stash"
+alias gsta 'git stash save'
+alias gstaa 'git stash apply'
+alias gstc 'git stash clear'
+alias gstd 'git stash drop'
+alias gstl 'git stash list'
+alias gstp 'git stash pop'
+alias gsts 'git stash show --text'
+alias gsu='git submodule update'
+##版本控制 hg 别名设置
+alias hgs 'hg status'
+alias hgu 'hg update'
+alias hgpl 'hg pull'
+alias hgpu 'hg push'
+alias hgc 'hg clone'
+
+##cd 别名设置
+alias cd.. 'cd ../'                         # Go back 1 directory level (for fast typers)
+alias cd. 'cd ..'
+alias .. 'cd ../'                           # Go back 1 directory level
+alias ... 'cd ../../'                       # Go back 2 directory levels
+alias .3 'cd ../../../'                     # Go back 3 directory levels
+alias .4 'cd ../../../../'                  # Go back 4 directory levels
+alias .5 'cd ../../../../../'               # Go back 5 directory levels
+alias .6 'cd ../../../../../../'            # Go back 6 directory levels
+alias .... 'cd ../../..'
+alias ..... 'cd ../../../..'
+alias ...... 'cd ../../../../..'
+
+##解压 别名设置
+alias gz='tar -zxvf'
+alias tgz='tar -zxvf'
+alias bz2='tar -xjvf'
+
+
 ##图片脚本设置
 sh {$MYRUNTIME}/customs/bin/extendslocatetochangepicurl
 
