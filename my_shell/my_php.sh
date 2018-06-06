0c56458add71d89aff38ad6a5cf64cc7cac1f850
杨浩 <yh392261226@gmail.com>
2018-06-06 09:38:59 +0800

    modified start command

--- my_shell/my_php.sh
+++ my_shell/my_php.sh
@@ -1,4 +1,4 @@
-export PATH="$(brew --prefix homebrew/php/php71)/bin:$(brew --prefix homebrew/php/php71)/sbin:$PATH"
+#export PATH="$(brew --prefix homebrew/core/php@72)/bin:$(brew --prefix homebrew/core/php@72)/sbin:$PATH"
 ### add php-school
 export PATH="$PATH:/Users/json/.php-school/bin"
 source $(brew --prefix php-version)/php-version.sh && php-version 7
