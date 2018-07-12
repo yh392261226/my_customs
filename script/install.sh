2c456ac822fcd256ab9a346664efaf5b40e1aeb3
空格不在 <yh392261226@sina.com>
2018-07-12 11:05:30 +0800

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -6,7 +6,7 @@ RUNTIMEPATH="$HOME/.runtime"
 #install brew first
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-[[ ! -f ~/.myruntime ]] && touch ~/.myruntime && echo $RUNTIMEPATH > ~/.myruntime
+[[ ! -f $HOME/.myruntime ]] && touch $HOME/.myruntime && echo $RUNTIMEPATH > $HOME/.myruntime
 ln -sf $HOME/Pictures $RUNTIMEPATH/pictures
 
 #install zshell & fish shell
@@ -23,29 +23,29 @@ brew install macvim --with-lua
 
 #install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-echo 'source $RUNTIMEPATH/customs/my_shell/rcfile' >> ~/.zhsrc
+git clone https://github.com/bhilburn/powerlevel9k.git $HOME/.oh-my-zsh/custom/themes/powerlevel9k
+echo 'source $RUNTIMEPATH/customs/my_shell/rcfile' >> $HOME/.zhsrc
 if [ -z fc-cache ]; then
   wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
   wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-  mv PowerlineSymbols.otf ~/.local/share/fonts/
-  fc-cache -vf ~/.local/share/fonts/
-  mkdir -p ~/.config/fontconfig/conf.d/
-  mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+  mv PowerlineSymbols.otf $HOME/.local/share/fonts/
+  fc-cache -vf $HOME/.local/share/fonts/
+  mkdir -p $HOME/.config/fontconfig/conf.d/
+  mv 10-powerline-symbols.conf $HOME/.config/fontconfig/conf.d/
 fi
 
 #install oh-my-fish
 curl -L https://get.oh-my.fish | fish
-echo 'source $RUNTIMEPATH/customs/my_shell/fish/zmy_basic.fish' >> ~/.config/fish/conf.d/omf.fish
-echo 'source $RUNTIMEPATH/customs/my_shell/fish/zmy_bindkeys.fish' >> ~/.config/fish/conf.d/omf.fish
-echo 'source $RUNTIMEPATH/customs/my_shell/fish/zmy_changebg.fish' >> ~/.config/fish/conf.d/omf.fish
-ln -sf $RUNTIMEPATH/customs/my_shell/fish/zmy_bindkeys.fish ~/.config/fish/functions/zmy_bindkeys.fish
+echo 'source $RUNTIMEPATH/customs/my_shell/fish/zmy_basic.fish' >> $HOME/.config/fish/conf.d/omf.fish
+echo 'source $RUNTIMEPATH/customs/my_shell/fish/zmy_bindkeys.fish' >> $HOME/.config/fish/conf.d/omf.fish
+echo 'source $RUNTIMEPATH/customs/my_shell/fish/zmy_changebg.fish' >> $HOME/.config/fish/conf.d/omf.fish
+ln -sf $RUNTIMEPATH/customs/my_shell/fish/zmy_bindkeys.fish $HOME/.config/fish/functions/zmy_bindkeys.fish
 #add fish to shells
 sudo echo "/usr/local/bin/fish" >> /etc/shells
 
 #install bash-it
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh
+git clone --depth=1 https://github.com/Bash-it/bash-it.git $HOME/.bash_it
+$HOME/.bash_it/install.sh
 if [ ! -f $HOME/.bashrc ]; then
   ln -sf $HOME/.bash_profile $HOME/.bashrc
 fi
@@ -55,7 +55,7 @@ sudo mkdir /tools/
 sudo chmod 777 /tools
 sudo chown $(whoami) /tools
 ln -sf $RUNTIMEPATH/customs/bin/ssh-auto-login /tools/ssh-auto-login
-cd ~/.runtime/tools/ 
+cd $RUNTIMEPATH/tools/ 
 [[ ! -f current_picture ]] && touch current_picture
 [[ ! -f current_picturename ]] && touch current_picturename
 [[ ! -f fpmark ]] && touch fpmark
