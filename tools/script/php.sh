856a7146f650144d9e45e44c6a5da5efc8fbca2f
石凡 <531992645@qq.com>
2018-07-13 15:51:58 +0800

    add php.sh

---
+++ tools/script/php.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone git://github.com/phpmd/phpmd.git $HOME/bin/phpmd
+
+git clone https://github.com/squizlabs/PHP_CodeSniffer.git $HOME/bin/phpcs
+
+git clone https://github.com/wilmoore/php-version.git $HOME/bin/php-version
