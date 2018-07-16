715250e66285d6041be4e099367a1b7df5de1475
wangjie <devsummer@126.com>
2018-07-16 08:50:46 +0800

    modified proxy

--- my_shell/my_proxy.sh
+++ my_shell/my_proxy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ###Desc:代理
-[[ -f $MYRUNTIME/tools/m_proxy ]] && source $MYRUNTIME/tools/m_proxy && export http_proxy=http://${ip}:${port};export https_proxy=http://${ip}:${port};
+switcher=0
+[[ -f $MYRUNTIME/tools/m_proxy ]] && source $MYRUNTIME/tools/m_proxy 
+[[ "$switcher" = "1" ]] && export http_proxy=http://${ip}:${port};export https_proxy=http://${ip}:${port};
