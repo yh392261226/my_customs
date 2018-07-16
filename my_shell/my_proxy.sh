1325c677895aad07d82fee0604ab532e3b50e986
石凡 <531992645@qq.com>
2018-07-16 08:47:12 +0800

    modify proxy

--- my_shell/my_proxy.sh
+++ my_shell/my_proxy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ###Desc:代理
-#export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;
+[[ -f $MYRUNTIME/tools/m_proxy ]] && source $MYRUNTIME/tools/m_proxy && export http_proxy=http://${ip}:${port};export https_proxy=http://${ip}:${port};
