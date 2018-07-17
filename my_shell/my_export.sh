122217fa28d81a88895c6c4722500aaf4201d532
石凡 <531992645@qq.com>
2018-07-17 14:39:03 +0800

    modified my_shell/my_export.sh

--- my_shell/my_export.sh
+++ my_shell/my_export.sh
@@ -1,17 +1,35 @@
 ### 环境变量设定
 export PLATFORM=$(uname -s)
 export BASE=$HOME
-export PATH="$(brew --prefix macvim)/bin:/usr/local/bin:$PATH"
-export RBENV_ROOT=/usr/local/var/rbenv
-export PATH="$(brew --prefix python3)/bin:$PATH"
-export PATH="/usr/local/opt/python@2/libexec/bin:$HOME/.cargo/bin:/opt/local/bin:$HOME/.local/bin:$PATH"
+[[ -d $(brew --prefix macvim) ]] && export PATH="$(brew --prefix macvim)/bin:/usr/local/bin:$PATH"
+[[ -d /usr/local/var/rbenv ]] && export RBENV_ROOT=/usr/local/var/rbenv
+[[ -d $(brew --prefix python3) ]] && export PATH="$(brew --prefix python3)/bin:$PATH"
+[[ -d /usr/local/opt/python@2/libexec/bin ]] && export PATH="/usr/local/opt/python@2/libexec/bin:$PATH"
+[[ -d $HOME/.cargo/bin ]] && export PATH="$HOME/.cargo/bin:/opt/local/bin:$PATH"
+[[ -d /opt/local/bin ]] && export PATH="/opt/local/bin:$PATH"
+[[ -d $HOME/.local/bin ]] && export PATH="$HOME/.local/bin:$PATH"
 eval "$(rbenv init -)"
 
 #####设置PATH变量
 if [ "$MYSYSNAME" = "Mac" ]; then
-    export PATH=/usr/local/bin:$PATH:/usr/local/var/rbenv/shims:/Users/json/go/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$HOME/.cabal/bin:/usr/local/sbin:$HOME/bin:$(brew --prefix go)/bin:/usr/local/heroku/bin:$MYRUNTIME/customs/bin:/usr/local/opt/llvm/bin
-    export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-    export PATH="$HOME/.Pokemon-Terminal:$PATH"
+    export PATH=
+    [[ -d /usr/local/bin ]] && export PATH="/usr/local/bin:$PATH"
+    [[ -d /usr/local/var/rbenv/shims ]] && export PATH="/usr/local/var/rbenv/shims:$PATH"
+    [[ -d $HOME/go/bin ]] && export PATH="$HOME/go/bin:$PATH"
+    [[ -d /usr/bin ]] && export PATH="/usr/bin:$PATH"
+    [[ -d /bin ]] && export PATH="/bin:$PATH"
+    [[ -d /usr/sbin ]] && export PATH="/usr/sbin:$PATH"
+    [[ -d /sbin ]] && export PATH="/sbin:$PATH"
+    [[ -d $HOME/.cabal/bin ]] && export PATH="$HOME/.cabal/bin:$PATH"
+    [[ -d /usr/local/sbin ]] && export PATH="/usr/local/sbin:$PATH"
+    [[ -d $HOME/bin ]] && export PATH="$HOME/bin:$PATH"
+    [[ -d $(brew --prefix go)/bin ]] && export PATH="$(brew --prefix go)/bin:$PATH"
+    [[ -d /usr/local/heroku/bin ]] && export PATH="/usr/local/heroku/bin:$PATH"
+    [[ -d $MYRUNTIME/customs/bin ]] && export PATH="$MYRUNTIME/customs/bin:$PATH"
+    [[ -d /usr/local/opt/llvm/bin ]] && export PATH="/usr/local/opt/llvm/bin:$PATH"
+    [[ -d /usr/local/opt/coreutils/libexec/gnubin ]] && export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+    [[ -d $HOME/.Pokemon-Terminal ]] && export PATH="$HOME/.Pokemon-Terminal:$PATH"
+    [[ -d /usr/local/anaconda3/bin ]] && export PATH="/usr/local/anaconda3/bin/:$PATH"
 else
     export PATH=$PATH:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/local/sbin:/usr/local/rvm/bin:$HOME/.cabal/bin
 fi
