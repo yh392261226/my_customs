82198c475736917f49782faa6036f01478e2e604
石凡 <531992645@qq.com>
2018-07-19 08:29:05 +0800

    modified proxy

--- my_shell/my_proxy.sh
+++ my_shell/my_proxy.sh
@@ -2,4 +2,4 @@
 ###Desc:代理
 switcher=0
 [[ -f $MYRUNTIME/tools/m_proxy ]] && source $MYRUNTIME/tools/m_proxy 
-[[ "$switcher" = "1" ]] && export http_proxy=http://${ip}:${port};export https_proxy=http://${ip}:${port};
+[[ "$switcher" = "1" ]] && export http_proxy=http://${ip}:${port} && export https_proxy=http://${ip}:${port}
