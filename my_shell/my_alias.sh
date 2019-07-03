7509c749331aefb656ca404dc945b8eedb72b4ea
杨浩 <yh392261226@gmail.com>
2019-07-03 13:07:40 +0800

    update fish

--- my_shell/my_alias.sh
+++ my_shell/my_alias.sh
@@ -82,6 +82,7 @@ alias ipInfo0='ipconfig getpacket en0'              # ipInfo0:      Get info on 
 alias ipInfo1='ipconfig getpacket en1'              # ipInfo1:      Get info on connections for en1
 alias openPorts='sudo lsof -i | grep LISTEN'        # openPorts:    All listening connections
 alias showBlocked='sudo ipfw list'                  # showBlocked:  All ipfw rules inc/ blocked IPs
+alias pping='prettyping'                            # a nice way to show ping command result
 
 #versions
 
