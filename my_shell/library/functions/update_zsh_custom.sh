e5d85388e3651c403cfc457aa4a346ca11fee307
杨浩 <yh392261226@gmail.com>
2019-07-24 09:29:32 +0800

    add image cat from url

--- my_shell/library/functions/update_zsh_custom.sh
+++ my_shell/library/functions/update_zsh_custom.sh
@@ -2,5 +2,5 @@
 function upzshcustoms() {
     upgitfiles $MYRUNTIME/oh-my-zsh/custom/plugins
     upgitfiles $MYRUNTIME/oh-my-zsh/antigen
-    upgitfiles $MYRUNTIME/oh-my-zsh/custom/themes/powerlevel9k
+#    upgitfiles $MYRUNTIME/oh-my-zsh/custom/themes/powerlevel9k
 }
