9b7dcb5c4cfdd789b866b3f09680330f32dbe76f
杨浩 <yh392261226@gmail.com>
2019-09-22 21:21:54 +0800

    update something

--- my_shell/library/functions/show_alias_list.sh
+++ my_shell/library/functions/show_alias_list.sh
@@ -1,7 +1,8 @@
 # Desc: 显示所有自定义命令及注释
 function showaliases() {
     MYRUNTIME=$(cat $HOME/.myruntime)
-    customcd $MYRUNTIME/customs/my_shell/library/functions; ls *.sh| fzf --no-sort --tac --toggle-sort=ctrl-r --height 95% --reverse --preview 'cat {}' --preview-window right:140
+    #customcd $MYRUNTIME/customs/my_shell/library/functions; ls *.sh| fzf --no-sort --tac --toggle-sort=ctrl-r --height 95% --reverse --preview 'cat {}' --preview-window right:140
+    cd $MYRUNTIME/customs/my_shell/library/functions; ls *.sh| fzf --no-sort --tac --toggle-sort=ctrl-r --height 95% --reverse --preview 'cat {}' --preview-window right:140
     # touch /tmp/tmp_all_aliases.log
     # for file in $(ls $MYRUNTIME/customs/my_shell/library/functions/*.sh); do
     #     cat $file | grep '^function ' | awk '{print "Command: " $2}' | sed 's/()//' | sed 's/{//' >> /tmp/tmp_all_aliases.log
