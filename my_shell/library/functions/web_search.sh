e80437a2e9c1b52940c95440a255d5bf6f4c05ab
杨浩 <yh392261226@gmail.com>
2021-01-06 17:19:07 +0800

    modify catw/cdw/llw/cdto/batw

--- my_shell/library/functions/web_search.sh
+++ my_shell/library/functions/web_search.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
-#Desc: 搜索引擎搜索 cbaidu|cgoogle keywords
+#Desc: 搜索引擎搜索 sbaidu|sgoogle keywords
 SEARCH_BROWSER="/Applications/Safari.app"
 [[ -d "/Applications/Google Chrome.app" ]] && SEARCH_BROWSER="/Applications/Google Chrome.app"
 
-function cgoogle() {
+function sgoogle() {
     echo "open -a $SEARCH_BROWSER https://www.google.com/search?q= $1";
     open -a "$SEARCH_BROWSER" "https://www.google.com/search?q= $1"；
 }
 
-function cbaidu() {
+function sbaidu() {
     echo "open -a $SEARCH_BROWSER https://www.baidu.com/s?wd= $1";
     open -a "$SEARCH_BROWSER" "https://www.baidu.com/s?wd= $1";
 }
 
-function bing() {
+function sbing() {
     echo "open -a $SEARCH_BROWSER http://www.bing.com/search?q= $1";
     open -a "$SEARCH_BROWSER" "http://www.bing.com/search?q= $1";
 }
 
-function yahoo() {
+function syahoo() {
     echo "open -a $SEARCH_BROWSER http://www.yahoo.com/search?q= $1";
     open -a "$SEARCH_BROWSER" "http://www.yahoo.com/search?q= $1";
 }
 
-function wikipedia() {
+function swikipedia() {
     echo "open -a $SEARCH_BROWSER http://en.wikipedia.org/wiki/Special:Search?search= $1";
     open -a "$SEARCH_BROWSER"  "http://en.wikipedia.org/wiki/Special:Search?search= $1";
 }
--- my_shell/library/functions/which_vim.sh
+++ my_shell/library/functions/which_vim.sh
@@ -1,4 +1,10 @@
 # Desc: vim 编辑which命令找到的文件地址
 function viw() {
-    vim `which "$1"`
+    command -v "$@" > /dev/null 2>&1
+    [[ "$?" = "1" ]] && echo "Command $@ does not exists !" && return 1
+    if [ "$(type $1 | grep 'a shell function from')" = "" ]; then
+        vim `which "$1"`
+    else
+        vim $(type "$1" | awk '{print $NF}')
+    fi
 }
