e4d5dddf95c991211358d301b140977cd42fbe3a
杨浩 <yh392261226@gmail.com>
2021-01-10 13:35:26 +0800

    modify files and direcotry

--- my_shell/my_func.sh
+++ my_shell/my_func.sh
@@ -1,4 +1,12 @@
 MYRUNTIME=$(cat $HOME/.myruntime)
-for func in $(ls $MYRUNTIME/customs/my_shell/library/functions/*.sh); do
+for func in $(ls $MYRUNTIME/customs/my_shell/library/functions/func_*.sh); do
     source $func
+done
+
+for core in $(ls $MYRUNTIME/customs/my_shell/library/core/core_*.sh); do
+    source $core
+done
+
+for third in $(ls $MYRUNTIME/customs/my_shell/library/third/third_*.sh); do
+    source $third
 done
