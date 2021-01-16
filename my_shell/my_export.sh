937e7dcdda63323701b062247c4ae227afa7a44f
杨浩 <yh392261226@gmail.com>
2021-01-16 16:20:14 +0800

    add ls_colors

--- my_shell/my_export.sh
+++ my_shell/my_export.sh
@@ -53,6 +53,26 @@ export WINEDEBUG=-all
 export MYCUSTOMS=$MYRUNTIME/customs
 export MYTOOLS=$MYRUNTIME/tools
 
+#Solarized Light
 export FZF_DEFAULT_OPTS='
 --color fg:240,bg:230,hl:33,fg+:241,bg+:221,hl+:33
 --color info:33,prompt:33,pointer:166,marker:166,spinner:33'
+#Molokai
+export FZF_DEFAULT_OPTS='--color fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81
+--color info:144,prompt:161,spinner:135,pointer:135,marker:118'
+#Red
+export FZF_DEFAULT_OPTS='--color fg:124,bg:16,hl:202,fg+:214,bg+:52,hl+:231
+--color info:52,prompt:196,spinner:208,pointer:196,marker:208'
+#Solarized Dark
+export FZF_DEFAULT_OPTS='--color dark,hl:33,hl+:37,fg+:235,bg+:136,fg+:254
+--color info:254,prompt:37,spinner:108,pointer:235,marker:235'
+#Nord
+export FZF_DEFAULT_OPTS='--color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
+--color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B'
+#Dracula
+export FZF_DEFAULT_OPTS='--color=dark
+--color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f
+--color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7'
+#Gruvbox Dark
+#export FZF_DEFAULT_OPTS='--color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f
+#  --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54'
