708167283ceacf04911fc39d4a8f6f4375b90611
杨浩 <yh392261226@gmail.com>
2021-02-15 22:29:19 +0800

    update fucking iterm2 shell integration

---
+++ my_shell/my_python.sh
@@ -0,0 +1,18 @@
+if [ -d $HOME/Library/Python/2.7/bin/ ]; then
+    export PATH=$PATH:$HOME/Library/Python/2.7/bin
+fi
+if [ -d $HOME/Library/Python/3.5/bin/ ]; then
+    export PATH=$PATH:$HOME/Library/Python/3.5/bin
+fi
+if [ -d $HOME/Library/Python/3.6/bin/ ]; then
+    export PATH=$PATH:$HOME/Library/Python/3.6/bin
+fi
+if [ -d $HOME/Library/Python/3.7/bin/ ]; then
+    export PATH=$PATH:$HOME/Library/Python/3.7/bin
+fi
+if [ -d $HOME/Library/Python/3.8/bin/ ]; then
+    export PATH=$PATH:$HOME/Library/Python/3.8/bin
+fi
+if [ -d $HOME/Library/Python/3.9/bin/ ]; then
+    export PATH=$PATH:$HOME/Library/Python/3.9/bin
+fi
