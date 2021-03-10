e160acf4925da7e8d6106fc42f471604c0ddf9ff
杨浩 <yh392261226@gmail.com>
2021-03-10 20:34:25 +0800

    ???

--- my_shell/library/functions/func_net.sh
+++ my_shell/library/functions/func_net.sh
@@ -58,7 +58,11 @@ function unsetproxy() { # Desc: unsetproxy:取消设置命令行代理
 }
 
 function getproxy() { # Desc: getproxy:获取命令行代理
-    echo "HTTP_PROXY: $HTTP_PROXY"
-    echo "HTTPS_PROXY: $HTTPS_PROXY"
-    echo "ALL_PROXY: $ALL_PROXY"
+    echo " \n
+    HTTP_PROXY: $HTTP_PROXY\n
+    HTTPS_PROXY: $HTTPS_PROXY\n
+    ALL_PROXY: $ALL_PROXY\n
+    \n
+    \n
+    " | cowsay | lolcat
 }
