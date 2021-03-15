e576d1731d596dd3be7561f318ee438de4849020
杨浩 <yh392261226@gmail.com>
2021-03-15 11:18:40 +0800

    ???

--- bin/ssh-auto-login/files/auto_login.sh
+++ bin/ssh-auto-login/files/auto_login.sh
@@ -7,9 +7,12 @@ if [ "" != "$4" ]; then
 	port=$4
 fi
 
+rows=$(stty size | awk '{print $1}')
+columns=$(stty size | awk '{print $2}')
+
 file=$HOME"/.ssh/master-$user@$host:$port"
 if [ -e "$file" ]; then
 	ssh $user@$host -p $port
 else
-	./auto_login.exp $host $user $password $save_RSA_key $port
+	./auto_login.exp $host $user $password $save_RSA_key $port $rows $columns
 fi
