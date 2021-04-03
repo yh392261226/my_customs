289b89e9986d3819f1e3586229e787f73608bc7d
杨浩 <yh392261226@gmail.com>
2021-04-03 18:57:50 +0800

    ???

--- bin/ssh-auto-login/files/auto_login.sh
+++ bin/ssh-auto-login/files/auto_login.sh
@@ -3,14 +3,16 @@ host=$1
 user=$2
 password=$3
 port=22
-if [ "" != "$4" ]; then
-	port=$4
-fi
+host_name=$5
+[[ "" != "$4" ]] && port=$4
+[[ "" != "$5" ]] && host_name=$5
 
 rows=$(stty size | awk '{print $1}')
 columns=$(stty size | awk '{print $2}')
 
+source $MYRUNTIME/customs/my_shell/my_common.sh
 file=$HOME"/.ssh/tmp/master-$user@$host:$port"
+success "已登录 $host_name"
 if [ -e "$file" ]; then
 	echo -e "\033[41;36m Reuse $user@$host:$port \033[0m" 
 	ssh $user@$host -p $port
@@ -18,5 +20,4 @@ else
 	cd $MYRUNTIME/customs/bin/ssh-auto-login/files
 	./auto_login.exp $host $user $password $save_RSA_key $port $rows $columns
 fi
-source $MYRUNTIME/customs/my_shell/my_common.sh
-success "已退出 $host"
+success "已退出 $host_name"
