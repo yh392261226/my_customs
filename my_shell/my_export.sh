760ed91a22f84acd6c9504f61f3e91d525fa4512
杨浩 <yh392261226@gmail.com>
2021-04-05 22:29:00 +0800

    ???

--- my_shell/my_export.sh
+++ my_shell/my_export.sh
@@ -85,4 +85,4 @@ export FZF_DEFAULT_OPTS='--color=dark
 #Gruvbox Dark
 #export FZF_DEFAULT_OPTS='--color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f
 #  --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54'
-export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --preview '(highlight -O bat --color=always {} || cat {}) 2> /dev/null | head -500'"
+export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --preview '(bat --color=always {} || ls {}) 2> /dev/null | head -500'"
