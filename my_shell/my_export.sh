56e11c0143e76aa5c01ae5b56362a29b11009914
杨浩 <yh392261226@gmail.com>
2021-08-24 14:01:54 +0800

    modify fzf opts

--- my_shell/my_export.sh
+++ my_shell/my_export.sh
@@ -66,8 +66,8 @@ export MYBIN=$MYCUSTOMS/bin
 
 FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build}"
 
-export FZF_DEFAULT_OPTS="--height 60% --multi --cycle --inline-info --ansi --border --layout=reverse --preview '(bat --theme=gruvbox-dark --color=always --style=header,grid --line-range :600 {} || exa {}) 2> /dev/null | head -500'  --preview-window right:70%:noborder --color 'fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81,info:144,prompt:161,spinner:135,pointer:135,marker:118,border:254'"
+export FZF_DEFAULT_OPTS="--height 60% --multi --cycle --inline-info --ansi --border --layout=reverse --preview '(bat --theme=gruvbox-dark --color=always --style=header,grid,numbers --line-range :600 {} || exa {}) 2> /dev/null | head -500'  --preview-window right:70%:noborder --color 'fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81,info:144,prompt:161,spinner:135,pointer:135,marker:118,border:254'"
 export FZF_COMPLETION_OPTS="-1 --cycle --inline-info --ansi --height 60% --border --layout=reverse --preview '$PREVIEW {}' --preview-window 'right:70%:wrap'  $FZF_PREVIEW_KEY_BIND"
 
 export FZF_TAB_OPTS=(-1 --cycle --inline-info --ansi --height 40% --border --layout=reverse  --expect=/ --no-preview)
-export FZF_TAB_OPTS=(-1 --cycle --inline-info --ansi --height 40% --border --layout=reverse  --expect=/ --priview '(bat --theme=gruvbox-dark --color=always --style=header,grid --line-range :600 {} || exa {}) 2> /dev/null | head -500'  --preview-window right:70%:noborder --color 'fg:#bbccdd,fg+:#ddeeff,bg:#334455,preview-bg:#223344,border:#778899')
+export FZF_TAB_OPTS=(-1 --cycle --inline-info --ansi --height 40% --border --layout=reverse  --expect=/ --priview '(bat --theme=gruvbox-dark --color=always --style=header,grid,numbers --line-range :600 {} || exa {}) 2> /dev/null | head -500'  --preview-window right:70%:noborder --color 'fg:#bbccdd,fg+:#ddeeff,bg:#334455,preview-bg:#223344,border:#778899')
