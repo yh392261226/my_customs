83a1ccb47030ca45aaff1f1384870ac1f02e894c
空格不在 <yh392261226@sina.com>
2021-11-12 14:54:59 +0800

    update other

--- my_shell/my_library.sh
+++ my_shell/my_library.sh
@@ -1,16 +1,16 @@
 MYRUNTIME=$(cat $HOME/.myruntime)
-for func in $(ls $MYRUNTIME/customs/my_shell/library/functions/func_*.bzsh); do
+for func in $(ls $MYRUNTIME/customs/my_shell/library/functions/bzsh/func_*.bzsh); do
     source $func
 done
 
-for core in $(ls $MYRUNTIME/customs/my_shell/library/core/core_*.bzsh); do
+for core in $(ls $MYRUNTIME/customs/my_shell/library/core/bzsh/core_*.bzsh); do
     source $core
 done
 
-for third in $(ls $MYRUNTIME/customs/my_shell/library/third/third_*.bzsh); do
+for third in $(ls $MYRUNTIME/customs/my_shell/library/third/bzsh/third_*.bzsh); do
     source $third
 done
 
-for other in $(ls $MYRUNTIME/customs/my_shell/library/others/others_*.bzsh); do
+for other in $(ls $MYRUNTIME/customs/my_shell/library/others/bzsh/others_*.bzsh); do
     source $other
 done
