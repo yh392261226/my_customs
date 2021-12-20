34cc77b794990520ac600fc623d671d8d22d319c
空格不在 <yh392261226@sina.com>
2021-12-20 15:41:22 +0800

    update game command

--- my_shell/fish/zmy_func.fish
+++ my_shell/fish/zmy_func.fish
@@ -1,7 +1,7 @@
 function setproxy
-    export HTTP_PROXY=http://127.0.0.1:58591;
-    export HTTPS_PROXY=http://127.0.0.1:58591;
-    export ALL_PROXY=socks5://127.0.0.1:51837;
+    export HTTP_PROXY=http://127.0.0.1:7890;
+    export HTTPS_PROXY=http://127.0.0.1:7890;
+    export ALL_PROXY=socks5://127.0.0.1:7890;
 end
 
 function unsetproxy
---
+++ my_shell/zsh/zmy_zstyles.zsh
@@ -0,0 +1,21 @@
+zstyle ':fzf-tab:complete:*:*' fzf-preview 'less ${(Q)realpath}'
+export LESSOPEN='|~/.lessfilter %s'
+zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview \
+	'git diff $word | delta'|
+zstyle ':fzf-tab:complete:git-log:*' fzf-preview \
+	'git log --color=always $word'
+zstyle ':fzf-tab:complete:git-help:*' fzf-preview \
+	'git help $word | bat -plman --color=always'
+zstyle ':fzf-tab:complete:git-show:*' fzf-preview \
+	'case "$group" in
+	"commit tag") git show --color=always $word ;;
+	*) git show --color=always $word | delta ;;
+	esac'
+zstyle ':fzf-tab:complete:git-checkout:*' fzf-preview \
+	'case "$group" in
+	"modified file") git diff $word | delta ;;
+	"recent commit object name") git show --color=always $word | delta ;;
+	*) git log --color=always $word ;;
+	esac'
+zstyle ':fzf-tab:complete:(\\|)run-help:*' fzf-preview 'run-help $word'
+zstyle ':fzf-tab:complete:(\\|*/|)man:*' fzf-preview 'man $word'
