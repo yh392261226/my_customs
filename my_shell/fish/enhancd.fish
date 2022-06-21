fdc53c7fbdf944397b6abb2fb81329f30d9a4a1d
空格不在 <yh392261226@sina.com>
2022-06-21 17:17:16 +0800

    update enhancd setting

--- my_shell/fish/enhancd.fish
+++ my_shell/fish/enhancd.fish
@@ -1,3 +1,8 @@
 ### enhancd / ecd
 set -gx ENHANCD_COMMAND "ecd"
 set -gx ENHANCD_DIR "$HOME/.enhancd"
+set -gx ENHANCD_HOOK_AFTER_CD "lsd -l"
+set -gx ENHANCD_USE_FUZZY_MATCH "1"
+set -gx ENHANCD_COMPLETION_KEYBIND "^I"
+set -gx ENHANCD_COMPLETION_BEHAVIOR "default"
+set -gx ENHANCD_FILTER "/opt/homebrew/bin/peco:fzf:non-existing-filter"
