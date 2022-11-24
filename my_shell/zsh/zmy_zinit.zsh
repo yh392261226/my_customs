e690ebb2c2b089d6df35d5b5edfba205dc4bb97c
空格不在 <yh392261226@sina.com>
2022-11-24 10:02:45 +0800

    forgot

--- my_shell/zsh/zmy_zinit.zsh
+++ my_shell/zsh/zmy_zinit.zsh
@@ -60,73 +60,91 @@ if [ -f $MYRUNTIME/customs/others/zinit/zinit.zsh ]; then
     # zinit load  <repo/plugin> # Load with reporting/investigating.
     # zinit light <repo/plugin> # Load without reporting/investigating.
     # zinit snippet <URL>
-
     # zinit snippet https://gist.githubusercontent.com/hightemp/5071909/raw/
+#=============================================================================
 
-    zinit ice depth=1; zinit light romkatv/powerlevel10k
-    # 快速目录跳转
-    zinit ice lucid wait='1'
-    zinit light skywind3000/z.lua
-    zinit light paulirish/git-open
-    zinit light zsh-users/zsh-completions
-    zinit light zsh-users/zsh-autosuggestions
+    ###Customs
+    zinit ice lucid wait='0'
 
     # 加载 OMZ 框架及部分插件
     # zinit snippet OMZL::git.zsh
-    # zinit snippet OMZ::lib/git.zsh
-    zinit snippet OMZ::lib/history.zsh
-    zinit snippet OMZ::lib/key-bindings.zsh
-    zinit snippet OMZ::lib/clipboard.zsh
-    zinit snippet OMZ::lib/completion.zsh
-    zinit snippet OMZ::lib/theme-and-appearance.zsh
+    zinit snippet OMZL::history.zsh
+    zinit snippet OMZL::key-bindings.zsh
+    zinit snippet OMZL::clipboard.zsh
+    zinit snippet OMZL::completion.zsh
+    zinit snippet OMZL::theme-and-appearance.zsh
+    zinit snippet OMZL::theme-and-appearance.zsh
 
     zinit snippet OMZP::cp
-    # zinit snippet OMZP::brew
-    # zinit snippet OMZP::extract
+    zinit snippet OMZP::brew
     # zinit snippet OMZP::vi-mode
-    # zinit snippet OMZP::sublime
+    zinit snippet OMZP::sublime
     zinit snippet OMZP::gitignore
     zinit snippet OMZP::colored-man-pages
+    # zinit snippet OMZP::git/git.plugin.zsh
+    # zinit snippet OMZP::mvn/mvn.plugin.zsh
+    zinit snippet OMZP::sudo/sudo.plugin.zsh
+    zinit snippet OMZP::common-aliases/common-aliases.plugin.zsh
+    zinit snippet OMZP::colored-man-pages/colored-man-pages.plugin.zsh
+    # zinit snippet OMZP::extract
+    zinit snippet OMZP::pip/pip.plugin.zsh
+    zinit snippet OMZP::command-not-found/command-not-found.plugin.zsh
+    # zinit snippet OMZP::tmux/tmux.plugin.zsh
 
-    # zinit snippet OMZ::plugins/git/git.plugin.zsh
-    # zinit snippet OMZ::plugins/mvn/mvn.plugin.zsh
-    # zinit snippet OMZ::plugins/sudo/sudo.plugin.zsh
-    # zinit snippet OMZ::plugins/common-aliases/common-aliases.plugin.zsh
-    zinit snippet OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh
+    # 延迟加载
+    zinit ice lucid wait='1'
+    # 快速目录跳转
+    zinit light skywind3000/z.lua
+    zinit light zsh-users/zsh-completions
+    zinit light zsh-users/zsh-autosuggestions
+    zinit light zdharma-continuum/fast-syntax-highlighting
+    # zinit light changyuheng/zsh-interactive-cd
+    zinit light shengyou/codeception-zsh-plugin
+    zinit light zsh-users/zsh-syntax-highlighting
+    zinit light paoloantinori/hhighlighter
+    zinit light floor114/zsh-apple-touchbar
+    zinit light djui/alias-tips
+    zinit light zsh-users/zsh-history-substring-search
+    zinit light iam4x/zsh-iterm-touchbar
+    zinit light supercrabtree/k
+    zinit light wfxr/formarks
+    zinit light junegunn/fzf
+    zinit light chitoku-k/fzf-zsh-completions
+    zinit light amaya382/zsh-fzf-widgets
+    zinit light urbainvaes/fzf-marks
+    zinit light Aloxaf/fzf-tab
+    zinit light edouard-lopez/yeoman-zsh-plugin
+    zinit light yh392261226/git-extra-commands
+    zinit light horosgrisa/mysql-colorize
+    zinit light zdharma-continuum/zbrowse
+    zinit light zdharma-continuum/zui
+    zinit light kazhala/dotbare
+    zinit light wfxr/forgit
+    zinit light paulirish/git-open
 
-
-    zinit ice svn
-    zinit snippet OMZ::plugins/extract
-
+    # ASDF
+    if [ -d "$HOME/.asdf" ]; then
+    zinit ice wait lucid
+    zinit light asdf-vm/asdf
+    # OR
+    #   load_asdf() {
+    #     . $HOME/.asdf/asdf.sh
+    #   }
+    #
+    #   zinit light-mode wait lucid for \
+    #     atload'load_asdf' \
+    #       zdharma-continuum/null
+    fi
 
     function reload_zinit() {
         source $MYRUNTIME/customs/my_shell/zsh/zmy_zinit.zsh
     }
     alias reloadz="reload_zinit"
 
-    ###Customs
-    zinit load chitoku-k/fzf-zsh-completions
-    zinit load amaya382/zsh-fzf-widgets
 
-
-    zinit light zdharma-continuum/fast-syntax-highlighting
-    # zinit light changyuheng/zsh-interactive-cd
-    zinit light floor114/zsh-apple-touchbar
-    zinit light yh392261226/git-extra-commands
-    zinit light djui/alias-tips
-    zinit light zsh-users/zsh-history-substring-search
-    zinit light iam4x/zsh-iterm-touchbar
-    zinit light zsh-users/zsh-syntax-highlighting
-    zinit light paoloantinori/hhighlighter
-    zinit light supercrabtree/k
-    zinit light shengyou/codeception-zsh-plugin
-    zinit light wfxr/formarks
-    zinit light horosgrisa/mysql-colorize
-    zinit light edouard-lopez/yeoman-zsh-plugin
-    zinit light zdharma-continuum/zbrowse
-    zinit light zdharma-continuum/zui
-    zinit light urbainvaes/fzf-marks
-    zinit light Aloxaf/fzf-tab
-    zinit light wfxr/forgit
-    zinit light kazhala/dotbare
+    zinit ice lucid wait='2'
+    zinit ice svn
+    zinit ice depth=1; zinit light romkatv/powerlevel10k
 fi
+
+bindkey ',' autosuggest-accept
