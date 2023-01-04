6108b55ffe22cea75b429e7d9c2e90bcac61481d
空格不在 <yh392261226@sina.com>
2023-01-04 13:46:38 +0800

    forgot

--- my_shell/my_java.sh
+++ my_shell/my_java.sh
@@ -1,5 +1,7 @@
 JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-17.0.1.jdk/Contents/Home 
+JAVAHOME=$JAVA_HOME
 CLASS_PATH="$JAVA_HOME/lib"
+CLASSPATH="$JAVA_HOME/lib"
 PATH="$JAVA_HOME/bin:$PATH"
 if [ "$PLATFORM" = 'Darwin' ]; then
     # Desc: 设置环境变量JAVA_HOME
