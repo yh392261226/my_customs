5743447c6adb8ba50459ef6b15c340cc6e11f92c
空格不在 <yh392261226@sina.com>
2024-03-03 20:06:19 +0800

    forgot

--- my_shell/fish/zmy_alias.fish
+++ my_shell/fish/zmy_alias.fish
@@ -1,2 +1,3 @@
 alias cls="/opt/homebrew/bin/lsd"
 alias ding="/opt/homebrew/bin/pokemon"
+alias ls="/opt/homebrew/bin/exa"
--- my_shell/fish/zmy_others.fish
+++ my_shell/fish/zmy_others.fish
@@ -5,3 +5,5 @@ if test -d $HOME/.runtime/customs/others/fzf-help
   source $HOME/.runtime/customs/others/fzf-help/src/fzf-help.fish
   bind \ca fzf-help-widget
 end
+
+eval $($MYRUNTIME/customs/bin/theme "$($MYRUNTIME/customs/bin/theme -l|tail -n1)")
