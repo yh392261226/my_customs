0aaf00a3a82ac45a77d19476fe3c7bd0a17af1f1
空格不在 <yh392261226@sina.com>
2024-03-09 15:34:53 +0800

    forgot

--- my_shell/fish/zmy_func.fish
+++ my_shell/fish/zmy_func.fish
@@ -7,6 +7,7 @@ function setproxy
     export HTTPS_PROXY=http://127.0.0.1:58591;
     export ALL_PROXY=socks5://127.0.0.1:51837;
 end
+alias onproxy="setproxy"
 
 function unsetproxy
     export HTTP_PROXY=;
@@ -14,6 +15,7 @@ function unsetproxy
     export ALL_PROXY=;
 
 end
+alias offproxy="unsetproxy"
 
 function eproxy
     echo "HTTP_PROXY: $HTTP_PROXY";
