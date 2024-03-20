83a9d09c8f5cb62370def74bc09c173f1e94a40f
空格不在 <yh392261226@sina.com>
2024-03-20 13:59:27 +0800

    去掉python环境变量

--- my_shell/my_python.sh
+++ my_shell/my_python.sh
@@ -1,27 +1,27 @@
-if [ -d $HOME/Library/Python/2.7/bin/ ]; then
-    export PATH="$HOME/Library/Python/2.7/bin:$PATH"
-fi
-if [ -d $HOME/Library/Python/3.5/bin/ ]; then
-    export PATH="$HOME/Library/Python/3.5/bin:$PATH"
-fi
-if [ -d $HOME/Library/Python/3.6/bin/ ]; then
-    export PATH="$HOME/Library/Python/3.6/bin:$PATH"
-fi
-if [ -d $HOME/Library/Python/3.7/bin/ ]; then
-    export PATH="$HOME/Library/Python/3.7/bin:$PATH"
-fi
-if [ -d $HOME/Library/Python/3.8/bin/ ]; then
-    export PATH="$HOME/Library/Python/3.8/bin:$PATH"
-fi
-if [ -d $HOME/Library/Python/3.9/bin/ ]; then
-    export PATH="$HOME/Library/Python/3.9/bin:$PATH"
-fi
-if [ -d $HOME/Library/Python/3.10/bin/ ]; then
-    export PATH="$HOME/Library/Python/3.10/bin:$PATH"
-fi
-if [ -d $HOME/Library/Python/3.11/bin/ ]; then
-    export PATH="$HOME/Library/Python/3.11/bin:$PATH"
-fi
-if [ -d $HOME/Library/Python/3.12/bin/ ]; then
-    export PATH="$HOME/Library/Python/3.12/bin:$PATH"
-fi
+# if [ -d $HOME/Library/Python/2.7/bin/ ]; then
+#     export PATH="$HOME/Library/Python/2.7/bin:$PATH"
+# fi
+# if [ -d $HOME/Library/Python/3.5/bin/ ]; then
+#     export PATH="$HOME/Library/Python/3.5/bin:$PATH"
+# fi
+# if [ -d $HOME/Library/Python/3.6/bin/ ]; then
+#     export PATH="$HOME/Library/Python/3.6/bin:$PATH"
+# fi
+# if [ -d $HOME/Library/Python/3.7/bin/ ]; then
+#     export PATH="$HOME/Library/Python/3.7/bin:$PATH"
+# fi
+# if [ -d $HOME/Library/Python/3.8/bin/ ]; then
+#     export PATH="$HOME/Library/Python/3.8/bin:$PATH"
+# fi
+# if [ -d $HOME/Library/Python/3.9/bin/ ]; then
+#     export PATH="$HOME/Library/Python/3.9/bin:$PATH"
+# fi
+# if [ -d $HOME/Library/Python/3.10/bin/ ]; then
+#     export PATH="$HOME/Library/Python/3.10/bin:$PATH"
+# fi
+# if [ -d $HOME/Library/Python/3.11/bin/ ]; then
+#     export PATH="$HOME/Library/Python/3.11/bin:$PATH"
+# fi
+# if [ -d $HOME/Library/Python/3.12/bin/ ]; then
+#     export PATH="$HOME/Library/Python/3.12/bin:$PATH"
+# fi
