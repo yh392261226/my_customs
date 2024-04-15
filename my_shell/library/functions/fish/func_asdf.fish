93015fb1bb1a70b8da1f27c1b284f9fd60f4e289
空格不在 <yh392261226@sina.com>
2024-04-15 09:11:43 +0800

    fix fish git

--- my_shell/library/functions/fish/func_asdf.fish
+++ my_shell/library/functions/fish/func_asdf.fish
@@ -1,16 +1,16 @@
 function fzf_asdf_install
     # Desc: function: fzf_asdf_install:安装一个或多个版本的语言包 e.g. `vmi rust` # => fzf multimode, tab to mark, enter to install if no plugin is supplied (e.g. `vmi<CR>`), fzf will list them for you Mnemonic [V]ersion [M]anager [I]nstall
-    set lang $argv[1]
+    set -l lang $argv
 
-    if test -z $lang
-        set lang (asdf plugin-list | fzf $FZF_CUSTOM_PARAMS --preview-window bottom:6:rounded:hidden:wrap --preview-label='[ 语言版本 ]' --header="$(_buildFzfHeader '' 'fzf_asdf_install')")
+    if not test "$lang" = ""
+        set -l lang (asdf plugin-list | fzf $FZF_CUSTOM_PARAMS --preview-window bottom:6:rounded:hidden:wrap --preview-label='[ 语言版本 ]' --header="$(_buildFzfHeader '' 'fzf_asdf_install')")
     end
 
     if test -n $lang
         set versions (asdf list-all $lang | fzf -m $FZF_CUSTOM_PARAMS --preview-window bottom:6:rounded:hidden:wrap --preview-label='[ 语言版本 ]' --header="$(_buildFzfHeader '' 'fzf_asdf_install')")
-        if test -n $versions
-            for version in (echo $versions)
-                asdf install $lang $version
+        if not test "$versions" = ""
+            for v in (echo $versions)
+                asdf install $lang $v
             end
         end
     end
@@ -19,7 +19,7 @@ alias fai="fzf_asdf_install"
 
 function fzf_asdf_uninstall
     # Desc: function: fzf_asdf_uninstall:删除一个或多个版本的语言包 e.g. `vmi rust` # => fzf multimode, tab to mark, enter to remove if no plugin is supplied (e.g. `vmi<CR>`), fzf will list them for you Mnemonic [V]ersion [M]anager [C]lean
-    set lang $argv[1]
+    set -l lang $argv
 
     if test -z $lang
         set lang (asdf plugin-list | fzf $FZF_CUSTOM_PARAMS --preview-window bottom:6:rounded:hidden:wrap --preview-label='[ 语言版本 ]' --header="$(_buildFzfHeader '' 'fzf_asdf_uninstall')")
@@ -27,9 +27,9 @@ function fzf_asdf_uninstall
 
     if test -n $lang
         set versions (asdf list $lang | fzf -m $FZF_CUSTOM_PARAMS --preview-window bottom:6:rounded:hidden:wrap --preview-label='[ 语言版本 ]' --header="$(_buildFzfHeader '' 'fzf_asdf_uninstall')")
-        if test -n $versions
-            for version in (echo $versions)
-                asdf uninstall $lang $version
+        if not test "$versions" = ""
+            for v in (echo $versions)
+                asdf uninstall $lang $v
             end
         end
     end
--- my_shell/library/functions/fish/func_brew.fish
+++ my_shell/library/functions/fish/func_brew.fish
@@ -1,11 +1,12 @@
 function brew_clean_caches
     # Desc: function: brew_clean_caches:Brew Delete (one or multiple) Caches files of  mnemonic (e.g. uninstall)
-    customcd (brew --cache)
-    rm -f ./*phar ./*patch ./*diff ./*xz ./*gz ./*bz2 ./*zip ./*rock
-    rm -rf ./*git
-    customcd (brew --cache)/downloads
-    rm -f ./*phar ./*patch ./*diff ./*xz ./*gz ./*bz2 ./*zip ./*rock
+    set -l current_dir (pwd)
+    cd (brew --cache)
+    find . -maxdepth 1 -type f -exec rm -f {} +
+    cd (brew --cache)/downloads
+    find . -maxdepth 1 -type f -exec rm -f {} +
     echo "Common files already deleted, You have to clean other files manually!!!"
+    cd $current_dir
 end
 alias bcc="brew_clean_caches"
 
@@ -13,7 +14,7 @@ function fzf_brew_delete_by_select
     # Desc: function: fzf_brew_delete_by_select:Brew Delete (one or multiple) selected application(s) mnemonic (e.g. uninstall)
     set uninst (brew leaves | fzf -m $FZF_CUSTOM_PARAMS --preview='brew info {1} | bat --color=always --language=Markdown --style=plain' --header="$(_buildFzfHeader '' 'fzf_brew_delete_by_select')")
 
-    if test -n $uninst
+    if not test "$uninst" = ""
         for prog in (echo $uninst)
             brew uninstall $prog
         end
@@ -25,7 +26,7 @@ function fzf_brew_update_by_select
     # Desc: function: fzf_brew_update_by_select:Brew Update (one or multiple) selected application(s) mnemonic [B]rew [U]pdate [P]lugin
     set upd (brew leaves | fzf -m $FZF_CUSTOM_PARAMS --preview='brew info {1} | bat --color=always --language=Markdown --style=plain' --header="$(_buildFzfHeader '' 'fzf_brew_update_by_select')")
 
-    if test -n $upd
+    if not test "$upd" = ""
         set upd (echo $upd | sed 's/\n/ /g')
         brew upgrade $upd
     end
@@ -35,7 +36,7 @@ alias fbup="fzf_brew_update_by_select"
 function fzf_brew_upgrade_by_select
     # Desc: function: fzf_brew_upgrade_by_select:Brew upgrade (one or multiple)
     set upgrads (brew outdated | fzf -m $FZF_CUSTOM_PARAMS --preview='brew info {1} | bat --color=always --language=Markdown --style=plain' --header="$(_buildFzfHeader '' 'fzf_brew_upgrade_by_select')")
-    if test -n $upgrads
+    if not test "$upgrads" = ""
         set upgrades (echo $upgrads | sed 's/\n/ /g')
         brew upgrade $upgrades
     end
@@ -54,7 +55,7 @@ function fzf_brew_install_by_select
         --preview='brew info {1} | bat --color=always --language=Markdown --style=plain' \
         --header=(_buildFzfHeader '' 'fzf_brew_install_by_select'))
 
-    if test -n "$inst"
+    if not test "$inst" = ""
         for prog in (echo $inst)
             brew install $prog
         end
@@ -63,76 +64,73 @@ end
 alias fbis="fzf_brew_install_by_select"
 
 function fzf_brew_install_or_remove
-    # Desc: function: fzf_brew_install_or_remove: Fully manage brew installation and suppression, and then some.
-    set -l wait_click "echo '\n\e[34mPress any key to continue...' && read -rsk 1"
-    set -l jq_all '(. | map(.cask_tokens) | flatten | map(split("/")[-1] ~ " (cask)"))[], (. | map(.formula_names) | flatten)[]'
-    set -l jq_installed '(.formulae[] | .name), (.casks[] | .token ~ " (cask)")'
-    set -l tmp_file (mktemp)
-    trap "rm -f $tmp_file" EXIT
-    set -l reload "reload%switch (cat $tmp_file)
-        case install
-            echo Install mode
-            brew tap-info --json --installed | jq --raw-output '$jq_all' | sort
-        case '*'
-            echo Remove mode
-            brew info --json=v2 --installed | jq --raw-output '$jq_installed' | sort
-        end"
-    set -l state "cat $tmp_file"
-    set -l nextstate "execute-silent%switch (cat $tmp_file)
-        case install
-            echo rm > $tmp_file
-        case '*'
-            echo install > $tmp_file
-        end"
-    set -l bold "\e[1m"
-    set -l reset "\e[0m"
-    set -l italic "\e[3m"
-    set -l gray "\e[30m"
-    set -l c "\e[1;36m"
-    set -l d "\e[1;37m"
-    set -l help "$bold$c[B$d]{$d}rew $c[I$d]{$d}nteractive$reset
-    $italic Tab$reset     Switch between install mode and remove mode
-    $italic Enter$reset   Select formula or cask for installation or deletion (depends on mode)
-    $italic ctrl-c$reset  Show formula or cask installation [s]ource code
-    $italic ctrl-j$reset  Show formula or cask [J]SON information
-    $italic crtl-e$reset  [E]dit formula or cask source code
-    $italic crtl-y$reset  Copy application
-    $italic crtl-s$reset  Switch between sort or no-sort mode
-    $italic crtl-/$reset  Switch between show or hide preview window
-    $italic ?$reset       Help (this page)
-    $italic ESC$reset     Quit
-    It is also advised you use auto-updates, this can be done with:
-        brew autoupdate start --upgrade --cleanup --enable-notification
-    "
-    echo install > $tmp_file
-    echo "Install mode (? for help)"
-    brew tap-info --json --installed | jq --raw-output "$jq_all" | sort |
+  set -l wait_click "echo \n\e[34mPress any key to continue...; and read -rsk 1"
+  set -l jq_all '(. | map(.cask_tokens) | flatten | map(split("/")[-1] + " (cask)"))[], (. | map(.formula_names) | flatten)[]'
+  set -l jq_installed '(.formulae[] | .name), (.casks[] | .token + " (cask)")'
+  set -l tmp_file (mktemp)
+  trap "rm -f $tmp_file" EXIT
+  set -l reload "reload%switch (cat $tmp_file)
+    case install
+      echo Install mode; brew tap-info --json --installed | jq --raw-output '$jq_all' | sort
+    case '*'
+      echo Remove mode; brew info --json=v2 --installed | jq --raw-output '$jq_installed' | sort
+  end"
+  set -l state "cat $tmp_file"
+  set -l nextstate "execute-silent%switch (cat $tmp_file)
+    case install
+      echo rm > $tmp_file
+    case '*'
+      echo install > $tmp_file
+  end"
+  set -l bold "\e[1m"
+  set -l reset "\e[0m"
+  set -l italic "\e[3m"
+  set -l gray "\e[30m"
+  set -l c "\e[1;36m"
+  set -l d "\e[1;37m"
+  set -l help "{$bold}{$c}[{$d}B{$c}]rew {$c}[{$d}I{$c}]nteractive{$reset}
+  {$italic}Tab{$reset}     Switch between install mode and remove mode
+  {$italic}Enter{$reset}   Select formula or cask for installation or deletion (depends on mode)
+  {$italic}ctrl-c{$reset}  Show formula or cask installation [s]ource code
+  {$italic}ctrl-j{$reset}  Show formula or cask [J]SON information
+  {$italic}crtl-e{$reset}  [E]dit formula or cask source code
+  {$italic}crtl-y{$reset}  Copy application
+  {$italic}crtl-s{$reset}  Switch between sort or no-sort mode
+  {$italic}crtl-/{$reset}  Switch between show or hide preview window
+  {$italic}?{$reset}       Help (this page)
+  {$italic}ESC{$reset}     Quit
+  It is also advised you use auto-updates, this can be done with:
+      brew autoupdate start --upgrade --cleanup --enable-notification
+  "
+  echo install > $tmp_file
+  echo "Install mode (? for help); brew tap-info --json --installed | jq --raw-output '$jq_all' | sort" | begin
     fzf $FZF_CUSTOM_PARAMS \
         --preview-window right:70%:rounded:hidden:wrap \
-        --bind="enter:execute(
-            if test '{2}' = '(cask)'
-                brew ($state) --cask {1}
-            else
-                brew ($state) {1}
-            end
-            $wait_click)+$reload" \
-        --bind='ctrl-c:preview(
-            bat --color=always (brew edit --print-path {1}) --style=header
-        )' \
+        --bind="enter:execute( \
+        if test '{2}' = 'cask'
+          brew ($state) --cask {1}
+        else
+          brew ($state) {1}
+        end
+        $wait_click
+        $reload)" \
+        --bind='ctrl-c:preview \
+        bat --color=always (brew edit --print-path {1}) --style=header' \
         --bind="ctrl-j:preview:brew info --json=v2 {1} | jq '
-            (.formulae + .casks)[0] | with_entries(select(try (.value | length > 0)))
-        ' | bat --plain --language=json --color=always" \
-        --bind="ctrl-e:execute:
-            EDITOR='code --wait' brew edit {1}
-            bat --color=always --language=markdown --plain <<-MD
-                To install the formulae (or cask) you edited with your changes, use:
-
-                    brew reinstall --build-from-source {1}
-            MD
-            $wait_click" \
+        (.formulae + .casks)[0] | with_entries(select(try (.value | length > 0)))
+      ' | bat --plain --language=json --color=always" \
+        --bind="ctrl-e:execute \
+        EDITOR='code --wait' brew edit {1}
+        bat --color=always --language=markdown --plain <<-MD
+          To install the formulae (or cask) you edited with your changes, use:
+              brew reinstall --build-from-source {1}
+        MD
+        $wait_click" \
         --bind="tab:$nextstate+$reload" \
-        --bind="?:preview:printf '$help'" \
+        --bind="?:preview \
+        printf '$help'" \
         --preview='brew info {1} | bat --color=always --language=Markdown --style=plain' \
         --header="$(_buildFzfHeader '' 'fzf_brew_install_or_remove')"
+  end
 end
 alias fbm="fzf_brew_install_or_remove"
