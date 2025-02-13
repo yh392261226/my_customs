af7c68c1759906a21dd283c9c26fe99fe6caed42
空格不在 <yh392261226@sina.com>
2025-02-13 21:14:22 +0800

    update git modules

--- my_shell/zsh/zmy_iterm2.zsh
+++ my_shell/zsh/zmy_iterm2.zsh
@@ -1,2 +1,2 @@
-
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+# 该功能已经在core/bzsh/core_other.bzsh中实现
+# test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
