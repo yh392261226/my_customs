61fdc9069e56bffc337d1b2969ff9ec8dd773eb7
空格不在 <yh392261226@sina.com>
2025-08-21 21:10:50 +0800

    add command greader2 in golang, finished normal functions

--- others/greader2_golang/build.sh
+++ others/greader2_golang/build.sh
@@ -1,6 +1,6 @@
 MYRUNTIME=$(cat $HOME/.myruntime)
 rm -f go.mod go.sum
-go mod init greader2
+go mod init reader
 go mod tidy
-go build -o greader2
-mv greader2 $MYRUNTIME/customs/bin/greader2
+go build -o reader
+mv reader $MYRUNTIME/customs/bin/reader
