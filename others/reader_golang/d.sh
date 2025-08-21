0131defd47d99014d43b84ce12f23632b94d2156
空格不在 <yh392261226@sina.com>
2025-08-21 21:12:42 +0800

    add command greader2 in golang, finished normal functions

--- others/reader_golang/d.sh
+++ others/reader_golang/d.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-
+echo "" >record.log
 for file in $(ls ./*.go); do
-    echo "" >> record.log
-    echo $file >> record.log
-    echo "\n=======================\n" >> record.log
-    cat $file >> record.log
-    echo "" >> record.log
+	echo "" >>record.log
+	echo $file >>record.log
+	echo "\n=======================\n" >>record.log
+	cat $file >>record.log
+	echo "" >>record.log
 done
